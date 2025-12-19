7c634fe13ada5cfa52796de6f57ded879256a0ae
Ruslan Mansurov <ruslanways@gmail.com>
2025-12-19 15:23:20 +0100

    feat(users): added help text for bio and website fields in User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class User(AbstractUser):
     """Custom user model"""
-    bio = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=500, blank=True, help_text='A brief summary of who you are')
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    website = models.URLField(max_length=200, blank=True)
+    website = models.CharField(max_length=200, blank=True, help_text='Your personal website or Facebook/Instagram profile')
     
     class Meta:
         db_table = 'users'
